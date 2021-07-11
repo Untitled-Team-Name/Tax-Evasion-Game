3f05033a1cc345729e1fde216537baa3e1178498
Alex Deatherage <alexdeatherage4829@gmail.com>
2021-07-11 13:47:19 -0700

    Add game over screen.

--- Assets/Scripts/LoseScreen.cs
+++ Assets/Scripts/LoseScreen.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOver : MonoBehaviour
+public class LoseScreen : MonoBehaviour
 {
 
     public void ExitButton() {
